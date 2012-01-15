7a9d2d0b2cf951d318f300c4b3b8177f7ea5922b
Matt <matt@supermatty.com>
2012-01-15 07:21:14 +0000

    Add rainbow colourmap using downloaded hsvToRgb code.
    git-svn-id: https://svn/svn/MandelbrotCanvas@2460 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- mandelbrot.js
+++ mandelbrot.js
@@ -11,10 +11,37 @@ function drawHLine(imageData, x1, y1, x2, r, g, b, a) {
 	}
 }
 
+function ColourMapRainbow() {
+	// TODO: This needs to be smarter. Maybe logarithmic.
+	this.numGradations = 70;
+	this.colourMap = [];
+	this.genColourMap = function() {
+		var h = 0, s = 1, v = 1, i;
+		for (i = 0; i < this.numGradations; i++) {
+			var rgb = hsvToRgb(h + (i / this.numGradations), s, v);
+			this.colourMap[i] = [ rgb[0], rgb[1], rgb[2], 255 ];
+		}
+	};
+	this.makeColour = function(n, maxIter) {
+		// points in set are black
+		if (n == maxIter) {
+			return [0, 0, 0, 255];
+		}
+		// outside set, iteration count modulo entire colourmap size selects
+		// colour
+		return this.colourMap[n % this.colourMap.length];
+	};
+	this.genColourMap();
+}
+
 $(function() {
 	var canvas = $('#mandelbrot');
 	var context = canvas[0].getContext("2d");
 	var imageData = context.getImageData(0, 0, canvas.width(), canvas.height());
-	drawHLine(imageData, 1, 1, imageData.width, 255, 0, 0, 128);
+	var cmap = new ColourMapRainbow();
+	for (var i = 0; i < 200; i++) {
+		var colour = cmap.makeColour(i, 1000);
+		drawHLine(imageData, 1, i, imageData.width, colour[0], colour[1], colour[2], colour[3]);
+	}
 	context.putImageData(imageData, 0, 0);
 });
