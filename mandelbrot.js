1d7dd5a7ac824ba8e178f10282e369f0f29cbff2
Matt <matt@supermatty.com>
2013-03-30 04:17:43 +0000

    Move Mandelbrot class into separate file.
    git-svn-id: https://svn/svn/MandelbrotCanvas@2808 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

---
+++ mandelbrot.js
@@ -0,0 +1,227 @@
+/**
+ * A panel displaying the Mandelbrot set.
+ * 
+ * @param canvas
+ *            A jQuery wrapper around the HTML5 canvas element to draw into
+ * @param cmap
+ *            Name of the colourmap to use
+ * @param etCalcName
+ *            Name of the escape-time calculator to use
+ */
+function Mandelbrot(canvas, escapeTimeCalculators, etCalcName, colourMaps, cmapName, renderProgress) {
+	this.canvas = canvas;
+	this.escapeTimeCalculators = escapeTimeCalculators;
+	this.colourMaps = colourMaps;
+	this.renderProgress = renderProgress;
+	this.context = this.canvas[0].getContext("2d");
+	this.imageData = this.context.getImageData(0, 0, this.canvas.width(),
+			this.canvas.height());
+	this.centreRl = 0;
+	this.centreIm = 0;
+	this.scale = 5 / Math.min(this.imageData.width, this.imageData.height);
+	this.maxIter = 100;
+	this.normalised = true;
+	/**
+	 * A radius of 2 is mathematically sufficient (as any point whose modulus
+	 * exceeds two escapes to infinity). However, setting radius > 2 improves
+	 * the smoothness of the colouring.
+	 */
+	this.radius = 3;
+	this.setFractalType(etCalcName);
+	this.setColourMapName(cmapName);
+}
+$.extend(Mandelbrot.prototype, {
+	colToX : function(c) {
+		return (c + 0.5 - this.imageData.width / 2)
+				* this.scale + this.centreRl;
+	},
+	rowToY : function(r) {
+		// Inversion due to the canvas' inverted-Y co-ordinate system.
+		// The set is symmetrical, but the co-ordinates are shown to the user.
+		return -(r + 0.5 - this.imageData.height / 2)
+				* this.scale + this.centreIm;
+	},
+	makeColour: function(cmap, n, lastVal, power, maxIter, normalised) {
+		// points in set are black
+		if (n === maxIter) {
+			return [0, 0, 0, 255];
+		}
+		// Lazily generate this colourmap's table of colours
+		if (!cmap.colourMap.length) {
+			cmap.genColourMap();
+		}
+		// outside set, iteration count modulo entire colourmap size selects colour
+		if (!normalised) {
+			// Return an entry directly from the colour map
+			return cmap.colourMap[n % cmap.colourMap.length];
+		}
+		/**
+		 * Generate a fractional normalised iteration count, then use it to interpolate
+		 * between two neighbouring colour map entries.
+		 */
+		n = Math.max(0, n + 1 - Math.log(Math.log(lastVal)) / Math.log(power));
+		return interpolateColour(
+			cmap.colourMap[Math.floor(n) % cmap.colourMap.length],
+			cmap.colourMap[(Math.floor(n) + 1) % cmap.colourMap.length],
+			n - Math.floor(n)
+		);
+	},
+	/**
+	 * The below performs the calculations and redraws in
+	 * multiple calls to a function using setTimeout, so that
+	 * the browser can redraw the UI between calls.
+	 * TODO: Use web worker if available
+	 */
+	update : function() {
+		var bandHeightMin = 10, bandHeightMax = 20;
+		var r = 0, that = this, bandHeight = Math.max(
+			bandHeightMin,
+			Math.min(
+				bandHeightMax,
+				Math.floor(this.imageData.height / 10)
+			)
+		);
+		this.stop();
+		this.renderProgress.progressbar('option', 'value', 0);
+		function updateFunc(mandelbrot, myUpdateTimeout) {
+			var rowEnd = Math.min(r + bandHeight, mandelbrot.canvas.height()), c, x, y, et, colour, percent;
+			mandelbrot.imageData = mandelbrot.context.getImageData(
+				0, 0, mandelbrot.canvas.width(), mandelbrot.canvas.height()
+			);
+			for (; r < rowEnd; r++) {
+				for (c = 0; c < mandelbrot.imageData.width; c++) {
+					x = mandelbrot.colToX(c);
+					y = mandelbrot.rowToY(r);
+					et = mandelbrot.calc.escapeTime.call(
+						mandelbrot.calc, x, y,
+						mandelbrot.maxIter,
+						mandelbrot.radius,
+						mandelbrot.normalised
+					);
+					colour = mandelbrot.makeColour(
+						mandelbrot.cmap,
+						et[0], et[1], et[2],
+						mandelbrot.maxIter,
+						mandelbrot.normalised
+					);
+					if (mandelbrot.updateTimeout !== myUpdateTimeout) {
+						return; // Abort - no longer the current render thread
+					}
+					setPixel(
+						mandelbrot.imageData, c, r,
+						colour[0], colour[1], colour[2], colour[3]
+					);
+				}
+			}
+			// TODO: Only need to blit one scanline
+			mandelbrot.context.putImageData(mandelbrot.imageData, 0, 0);
+			percent = Math.floor((r * 100.0) / mandelbrot.imageData.height);
+			if (r < mandelbrot.canvas.height()) {
+				// TODO: Animate the progress bar smoothly.
+				// FIXME: This animates it, but all of the
+				// animation occurs after rendering is complete:
+				/*
+				 * $('.ui-progressbar-value').stop(true).animate({width:
+				 * percent + '%'}, 1000, function() {
+				 * this.renderProgress.progressbar('option', 'value',
+				 * percent); });
+				 */
+				mandelbrot.renderProgress.progressbar('option', 'value', percent);
+				mandelbrot.updateTimeout = setTimeout(function() {
+					updateFunc(mandelbrot, mandelbrot.updateTimeout);
+				});
+			} else {
+				mandelbrot.context.putImageData(mandelbrot.imageData, 0, 0);
+				mandelbrot.renderProgress.progressbar('option', 'value', 100);
+			}
+		}
+		this.updateTimeout = setTimeout(function() {
+			updateFunc(that, that.updateTimeout);
+		});
+	},
+	stop : function() {
+		clearTimeout(this.updateTimeout);
+		this.updateTimeout = null;
+	},
+	/**
+	 * NOTE: I would like to simply translate and scale the
+	 * canvas here; but at time of writing, there is no portable
+	 * way to retrieve the canvas' current transform matrix. So
+	 * I simply do my own, manual transforms. One could
+	 * implement (and some have) a polyfill for this: maintain a
+	 * 'shadow' copy of the canvas' current transform matrix,
+	 * over-ride every relevant canvas mutator so it
+	 * concatenates the newly-applied transform with the shadow
+	 * matrix, then regurgitate the shadow matrix on demand.
+	 * 
+	 * NOTE: No, that would not work. The Canvas' putImageData
+	 * method does not use the transformation matrix. You can
+	 * however draw a canvas onto another canvas, possibly with
+	 * transformation.
+	 */
+	getCentre : function() {
+		return [ this.centreRl, this.centreIm ];
+	},
+	setCentre : function(rl, im) {
+		this.centreRl = rl;
+		this.centreIm = im;
+	},
+	getScale : function() {
+		return this.scale;
+	},
+	setScale : function(newScale) {
+		this.scale = newScale;
+		return this.scale;
+	},
+	zoomBy : function(factor) {
+		this.scale *= factor;
+		return this.scale;
+	},
+	zoomInBy : function(factor) {
+		return this.zoomBy(1 / factor);
+	},
+	zoomOutBy : function(factor) {
+		return this.zoomBy(factor);
+	},
+	getMaxIter : function() {
+		return this.maxIter;
+	},
+	setMaxIter : function(newMaxIter) {
+		this.maxIter = newMaxIter;
+	},
+	getRadius : function() {
+		return this.radius;
+	},
+	setRadius : function(newRadius) {
+		this.radius = newRadius;
+	},
+	getColourMapName : function() {
+		return this.cmapName;
+	},
+	setColourMapName : function(newCmapName) {
+		if (!(newCmapName.toLowerCase() in this.colourMaps)) {
+			throw 'Unknown colour map name "' + newCmapName + "'";
+		}
+		this.cmapName = newCmapName;
+		this.cmap = this.colourMaps[newCmapName.toLowerCase()];
+	},
+	getFractalType : function() {
+		return this.etCalcName;
+	},
+	setFractalType : function(newCalcName) {
+		if (!(newCalcName.toLowerCase() in this.escapeTimeCalculators)) {
+			throw 'Unknown escape time calculator name "' + newCalcName + "'";
+		}
+		this.etCalcName = newCalcName;
+		this.calc = this.escapeTimeCalculators[newCalcName.toLowerCase()];
+	},
+	getFractalEquation : function() {
+		return this.calc.equation;
+	},
+	getNormalised : function() {
+		return this.normalised;
+	},
+	setNormalised : function(newNormalised) {
+		this.normalised = newNormalised;
+	}
+});
--- scripts.js
+++ scripts.js
@@ -15,31 +15,6 @@ jQuery(function() {
 			buttonStop = $('#stop'), displayName = $('#name'),
 			resizable = $('.resizable'),
 			renderProgress = $('#renderProgress');
-		function makeColour(cmap, n, lastVal, power, maxIter, normalised) {
-			// points in set are black
-			if (n === maxIter) {
-				return [0, 0, 0, 255];
-			}
-			// Lazily generate this colourmap's table of colours
-			if (!cmap.colourMap.length) {
-				cmap.genColourMap();
-			}
-			// outside set, iteration count modulo entire colourmap size selects colour
-			if (!normalised) {
-				// Return an entry directly from the colour map
-				return cmap.colourMap[n % cmap.colourMap.length];
-			}
-			/**
-			 * Generate a fractional normalised iteration count, then use it to interpolate
-			 * between two neighbouring colour map entries.
-			 */
-			n = Math.max(0, n + 1 - Math.log(Math.log(lastVal)) / Math.log(power));
-			return interpolateColour(
-				cmap.colourMap[Math.floor(n) % cmap.colourMap.length],
-				cmap.colourMap[(Math.floor(n) + 1) % cmap.colourMap.length],
-				n - Math.floor(n)
-			);
-		}
 		/**
 		 * Various ways of mapping escape-time values to a repeating range of colours.
 		 */
@@ -347,171 +322,6 @@ jQuery(function() {
 		    	}
 			}
 		};
-		/**
-		 * A panel displaying the Mandelbrot set.
-		 * @param canvas A jQuery wrapper around the HTML5 canvas element to draw into
-		 * @param cmap Name of the colourmap to use
-		 * @param etCalcName Name of the escape-time calculator to use
-		 */
-		function Mandelbrot(canvas, cmapName, etCalcName) {
-			this.canvas = canvas;
-			this.context = this.canvas[0].getContext("2d");
-			this.imageData = this.context.getImageData(0, 0, this.canvas.width(), this.canvas.height());
-			this.centreRl = 0;
-			this.centreIm = 0;
-			this.scale = 5 / Math.min(this.imageData.width, this.imageData.height);
-			this.maxIter = 100;
-			this.normalised = true;
-			/**
-			 * A radius of 2 is mathematically sufficient (as any point whose modulus exceeds two escapes to infinity).
-			 * However, setting radius > 2 improves the smoothness of the colouring.
-			 */
-			this.radius = 3;
-			this.setFractalType(etCalcName);
-			this.setColourMapName(cmapName);
-		}
-		$.extend(Mandelbrot.prototype, {
-			colToX: function(c) {
-				return  (c + 0.5 - this.imageData.width  / 2) * this.scale + this.centreRl;
-			},
-			rowToY: function(r) {
-				// Inversion due to the canvas' inverted-Y co-ordinate system.
-				// The set is symmetrical, but the co-ordinates are shown to the user.
-				return -(r + 0.5 - this.imageData.height / 2) * this.scale + this.centreIm;
-			},
-			/**
-			 * The below performs the calculations and redraws in multiple calls
-			 * to a function using setTimeout, so that the browser can redraw
-			 * the UI between calls.
-			 * TODO: Use web worker if available
-			 */
-			update: function() {
-				var bandHeightMin = 10, bandHeightMax = 20;
-				var r = 0, that = this, bandHeight = Math.max(bandHeightMin, Math.min(bandHeightMax, Math.floor(this.imageData.height / 10)));
-				this.stop();
-				renderProgress.progressbar('option', 'value', 0);
-				function updateFunc(mandelbrot, myUpdateTimeout) {
-					var rowEnd = Math.min(r + bandHeight, mandelbrot.canvas.height()), c, x, y, et, colour;
-					mandelbrot.imageData = mandelbrot.context.getImageData(0, 0, mandelbrot.canvas.width(), mandelbrot.canvas.height());
-					for (; r < rowEnd; r++) {
-						for (c = 0; c < mandelbrot.imageData.width; c++) {
-							x = mandelbrot.colToX(c);
-							y = mandelbrot.rowToY(r);
-							et = mandelbrot.calc.escapeTime.call(mandelbrot.calc, x, y, mandelbrot.maxIter, mandelbrot.radius, mandelbrot.normalised);
-							colour = makeColour(mandelbrot.cmap, et[0], et[1], et[2], mandelbrot.maxIter, mandelbrot.normalised);
-							if (mandelbrot.updateTimeout !== myUpdateTimeout) {
-								return; // Abort - no longer the current render thread
-							} 
-							setPixel(mandelbrot.imageData, c, r, colour[0], colour[1], colour[2], colour[3]);
-						}
-					}
-					// TODO: Only need to blit one scanline
-					mandelbrot.context.putImageData(mandelbrot.imageData, 0, 0);
-					var percent = Math.floor((r * 100.0) / mandelbrot.imageData.height);
-					if (r < mandelbrot.canvas.height()) {
-						// TODO: Animate the progress bar smoothly.
-						// FIXME: This animates it, but all of the animation occurs after rendering is complete:
-						/*
-						$('.ui-progressbar-value').stop(true).animate({width: percent + '%'}, 1000, function() {
-							renderProgress.progressbar('option', 'value', percent);
-						});
-						*/
-						renderProgress.progressbar('option', 'value', percent);
-						mandelbrot.updateTimeout = setTimeout(function() {
-							updateFunc(mandelbrot, mandelbrot.updateTimeout);
-						});
-					} else {
-						mandelbrot.context.putImageData(mandelbrot.imageData, 0, 0);
-						renderProgress.progressbar('option', 'value', 100);
-					}
-				}
-				this.updateTimeout = setTimeout(function() {
-					updateFunc(that, that.updateTimeout);
-				});
-			},
-			stop: function() {
-				clearTimeout(this.updateTimeout);
-				this.updateTimeout = null;
-			},
-			/**
-			 * NOTE: I would like to simply translate and scale the canvas here;
-			 * but at time of writing, there is no portable way to retrieve the canvas'
-			 * current transform matrix. So I simply do my own, manual transforms.
-			 * One could implement (and some have) a polyfill for this:
-			 * maintain a 'shadow' copy of the canvas' current transform matrix,
-			 * over-ride every relevant canvas mutator so it concatenates the newly-applied transform
-			 * with the shadow matrix, then regurgitate the shadow matrix on demand.
-			 * 
-			 * NOTE: No, that would not work.
-			 * The Canvas' putImageData method does not use the transformation matrix.
-			 * You can however draw a canvas onto another canvas, possibly with transformation.
-			 */
-			getCentre: function() {
-				return [ this.centreRl, this.centreIm ];
-			},
-			setCentre: function(rl, im) {
-				this.centreRl = rl;
-				this.centreIm = im;
-			},
-			getScale: function() {
-				return this.scale;
-			},
-			setScale: function(newScale) {
-				this.scale = newScale;
-				return this.scale;
-			},
-			zoomBy: function(factor) {
-				this.scale *= factor;
-				return this.scale;
-			},
-			zoomInBy: function(factor) {
-				return this.zoomBy(1 / factor);
-			},
-			zoomOutBy: function(factor) {
-				return this.zoomBy(factor);
-			},
-			getMaxIter: function() {
-				return this.maxIter;
-			},
-			setMaxIter: function(newMaxIter) {
-				this.maxIter = newMaxIter;
-			},
-			getRadius: function() {
-				return this.radius;
-			},
-			setRadius: function(newRadius) {
-				this.radius = newRadius;
-			},
-			getColourMapName: function() {
-				return this.cmapName;
-			},
-			setColourMapName: function(newCmapName) {
-				if (!(newCmapName.toLowerCase() in colourMaps)) {
-					throw 'Unknown colour map name "' + newCmapName + "'";
-				}
-				this.cmapName = newCmapName;
-				this.cmap = colourMaps[newCmapName.toLowerCase()];
-			},
-			getFractalType: function() {
-				return this.etCalcName;
-			},
-			setFractalType: function(newCalcName) {
-				if (!(newCalcName.toLowerCase() in escapeTimeCalculators)) {
-					throw 'Unknown escape time calculator name "' + newCalcName + "'";
-				}
-				this.etCalcName = newCalcName;
-				this.calc = escapeTimeCalculators[newCalcName.toLowerCase()];
-			},
-			getFractalEquation: function() {
-				return this.calc.equation;
-			},
-			getNormalised: function() {
-				return this.normalised;
-			},
-			setNormalised: function(newNormalised) {
-				this.normalised = newNormalised;
-			}
-		});
 		// Create a Mandelbrot set and controls
 		for (cmapName in colourMaps) {
 			// Generate an entry in the drop-down select list for this colour map
@@ -525,7 +335,7 @@ jQuery(function() {
 			option.text(ucFirstAll(etCalcName));
 			displayFractalType.append(option);
 		}
-		mandelbrot = new Mandelbrot(canvas, displayColourMap.val(), displayFractalType.val());
+		mandelbrot = new Mandelbrot(canvas, escapeTimeCalculators, displayFractalType.val(), colourMaps, displayColourMap.val(), renderProgress);
 		function updateControls() {
 			displayCentreRl.val(mandelbrot.getCentre()[0]);
 			displayCentreIm.val(mandelbrot.getCentre()[1]);
